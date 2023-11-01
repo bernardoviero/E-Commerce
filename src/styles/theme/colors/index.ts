93b65c6d1b5d34a5a7648d041ce66288055c6e54
BernardoViero <bernardo.viero@avmb.com.br>
2023-11-01 15:06:55 -0300

    finally task

---
+++ src/styles/theme/colors/index.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  primaryColor: '#5D5FEF',
+  secondaryColor: '#A5A6F6',
+  backgroundScreen: '#FFF',
+  backgroundInput: '#E9E9E9',
+  defautText: '#000',
+  greyText: '#545454',
+  placeholderInput: '#BDBEBD',
+};
